4d8e4f3b49bb254dc13d48844a0848ec2307e3da
Ankit Sadana <sadana.ankit@gmail.com>
2021-03-10 21:36:19 -0500

    Added a basic lambda with EventBridge rule

--- app.py
+++ app.py
@@ -2,16 +2,9 @@
 
 from aws_cdk import core as cdk
 
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
 from meme_picker.meme_picker_stack import MemePickerStack
 
-
 app = cdk.App()
-MemePickerStack(app, "meme-picker")
+MemePickerStack(app, "meme-picker", env={'region': 'us-east-1'})
 
 app.synth()
--- meme_picker/meme_picker_stack.py
+++ meme_picker/meme_picker_stack.py
@@ -1,10 +1,9 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    core as cdk,
+    aws_events as events,
+    aws_lambda as _lambda,
+    aws_events_targets as targets
+)
 
 
 class MemePickerStack(cdk.Stack):
@@ -12,4 +11,24 @@ class MemePickerStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Defines a Lambda
+        lambda_fn = _lambda.Function(
+            self, 'MemePickerLambda',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.asset('resources'),
+            handler='lambda_meme_picker.handler',
+            timeout=cdk.Duration.seconds(500),
+        )
+
+        # Run every Friday
+        # See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
+        rule = events.Rule(
+            self, "Rule",
+            schedule=events.Schedule.cron(
+                minute='0',
+                hour='0',
+                month='*',
+                week_day='FRI',
+                year='*'),
+        )
+        rule.add_target(targets.LambdaFunction(lambda_fn))
