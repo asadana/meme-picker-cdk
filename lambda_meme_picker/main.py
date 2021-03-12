8b98ba397f5fe2b3b2783f19880687de655003e5
Ankit Sadana <sadana.ankit@gmail.com>
2021-03-11 19:10:41 -0500

    (#2) Added logic to sanitize the image and convert to JPG

--- lambda_meme_picker/main.py
+++ lambda_meme_picker/main.py
@@ -4,20 +4,37 @@ import urllib.request
 
 import boto3
 
+from PIL import Image, UnidentifiedImageError
+
 s3 = boto3.resource('s3')
 bucket_name = os.getenv('S3_BUCKET_NAME')
+
+FILE_NAME = '/tmp/meme-of-the-week.jpg'
 
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
 
-    urllib.request.urlretrieve('https://www.memecreator.org/static/images/memes/4185030.jpg',
-                               '/tmp/meme-of-the-week.jpg')
+    path, _ = urllib.request.urlretrieve('https://i.pinimg.com/originals/4a/32/fb/4a32fbcba40cc06ea6ca0edbd4dbae1c.png')
 
-    s3.meta.client.upload_file('/tmp/meme-of-the-week.jpg', bucket_name, 'meme-of-the-week.jpg',
-                               ExtraArgs={
-                                   "ACL": "public-read"
-                               })
+    try:
+        with open(path, 'rb') as image_file:
+            image = Image.open(image_file)
+
+            # next 3 lines strip exif
+            image_data = list(image.getdata())
+            image_without_exif = Image.new(image.mode, image.size)
+            image_without_exif.putdata(image_data)
+
+            image_without_exif.convert('RGB')
+            image_without_exif.save(FILE_NAME)
+
+            s3.meta.client.upload_file(FILE_NAME, bucket_name, 'meme-of-the-week.jpg',
+                                       ExtraArgs={
+                                           "ACL": "public-read"
+                                       })
+    except UnidentifiedImageError:
+        print("The image download was unsuccessful.")
 
     return {
         'statusCode': 200,
